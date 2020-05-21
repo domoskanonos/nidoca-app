ac86fcd018849eb11b6e114959a3d60390e48b2b
Domoskanonos <Tgg8855tt#>
2020-05-21 22:27:18 +0200

    modified

--- source/component/i18n-selector.js
+++ source/component/i18n-selector.js
@@ -5,13 +5,13 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var I18NSelectorComponent_1;
+var NidocaI18NSelector_1;
 Object.defineProperty(exports, "__esModule", { value: true });
 const lit_element_1 = require("lit-element");
 const frontend_basis_1 = require("@domoskanonos/frontend-basis");
 const frontend_basis_2 = require("@domoskanonos/frontend-basis");
 const nidoca_core_1 = require("@domoskanonos/nidoca-core");
-let I18NSelectorComponent = I18NSelectorComponent_1 = class I18NSelectorComponent extends lit_element_1.LitElement {
+let NidocaI18NSelector = NidocaI18NSelector_1 = class NidocaI18NSelector extends lit_element_1.LitElement {
     constructor() {
         super(...arguments);
         this.label = '';
@@ -34,21 +34,21 @@ let I18NSelectorComponent = I18NSelectorComponent_1 = class I18NSelectorComponen
     changeLanguage(event) {
         this.langKey = event.detail.outputData.value[0];
         frontend_basis_1.I18nService.getUniqueInstance().setLanguage(this.langKey);
-        frontend_basis_2.BasicService.getUniqueInstance().dispatchSimpleCustomEvent(this, I18NSelectorComponent_1.EVENT_CHANGE_LANGUAGE, this.langKey);
+        frontend_basis_2.BasicService.getUniqueInstance().dispatchSimpleCustomEvent(this, NidocaI18NSelector_1.EVENT_CHANGE_LANGUAGE, this.langKey);
     }
 };
-I18NSelectorComponent.EVENT_CHANGE_LANGUAGE = 'nidoca-i18n-selector-change-language';
+NidocaI18NSelector.EVENT_CHANGE_LANGUAGE = 'nidoca-i18n-selector-change-language';
 __decorate([
     lit_element_1.property()
-], I18NSelectorComponent.prototype, "label", void 0);
+], NidocaI18NSelector.prototype, "label", void 0);
 __decorate([
     lit_element_1.property()
-], I18NSelectorComponent.prototype, "langKey", void 0);
+], NidocaI18NSelector.prototype, "langKey", void 0);
 __decorate([
     lit_element_1.property()
-], I18NSelectorComponent.prototype, "languages", void 0);
-I18NSelectorComponent = I18NSelectorComponent_1 = __decorate([
+], NidocaI18NSelector.prototype, "languages", void 0);
+NidocaI18NSelector = NidocaI18NSelector_1 = __decorate([
     lit_element_1.customElement('nidoca-i18n-selector')
-], I18NSelectorComponent);
-exports.I18NSelectorComponent = I18NSelectorComponent;
+], NidocaI18NSelector);
+exports.NidocaI18NSelector = NidocaI18NSelector;
 //# sourceMappingURL=i18n-selector.js.map
