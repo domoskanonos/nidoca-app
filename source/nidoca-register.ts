c8ead9c4590c7c50ce3d4a058f82450da59d957c
dominik.bruhn <dominikbruhn@googlemail.com>
2020-05-23 20:29:00 +0200

    modified

--- source/nidoca-register.ts
+++ source/nidoca-register.ts
@@ -1,110 +1,154 @@
-import { customElement, html, LitElement, property, query } from "lit-element";
+import { customElement, html, LitElement, property, query } from 'lit-element';
 import {
-  NidocaForm,
-  TypographyType,
-  InputfieldType,
-  NidocaFormOutputData
-} from "@domoskanonos/nidoca-core";
-import { BasicService, I18nService } from "@domoskanonos/frontend-basis";
+   NidocaForm,
+   TypographyType,
+   InputfieldType
+} from '@domoskanonos/nidoca-core';
+import { BasicService, I18nService } from '@domoskanonos/frontend-basis';
+import {
+   InputfieldMode,
+   SpacerAlignment,
+   SpacerSize,
+   TypographyAlignment,
+   VisibleType
+} from '@domoskanonos/nidoca-core/lib';
 
-@customElement("nidoca-register")
+@customElement('nidoca-register')
 export class NidocaRegister extends LitElement {
-  static EVENT_SUBMIT: string = "nidoca-register-submit";
+   @property()
+   errorMessage: string = '';
 
-  @property()
-  errorMessage: string = "";
+   @property()
+   hrefTermsOfUse: string = '';
 
-  @query("#email")
-  emailInputField: HTMLInputElement | undefined;
+   @property()
+   hrefLogin: string = '';
 
-  @query("#repeat-email")
-  repeatEmailInputField: HTMLInputElement | undefined;
+   @query('#email')
+   emailInputField: HTMLInputElement | undefined;
 
-  @query("#register-form")
-  formComponent: NidocaForm | undefined;
+   @query('#repeat-email')
+   repeatEmailInputField: HTMLInputElement | undefined;
 
-  render() {
-    return html`
-      <nidoca-form id="register-form">
-        <nidoca-typography .typographyType="${TypographyType.H4}"
-          >${I18nService.getUniqueInstance().getValue(
-            "component_register_header"
-          )}</nidoca-typography
-        >
-        <nidoca-inputfield
-          id="email"
-          name="email"
-          .inputfieldType="${InputfieldType.EMAIL}"
-          label="${I18nService.getUniqueInstance().getValue(
-            "component_register_email"
-          )}"
-          trailingIcon="mail"
-          required="true"
-        ></nidoca-inputfield>
-        <nidoca-inputfield
-          id="repeat-email"
-          name="repeatEmail"
-          .inputfieldType="${InputfieldType.EMAIL}"
-          label="${I18nService.getUniqueInstance().getValue(
-            "component_register_repeat_email"
-          )}"
-          trailingIcon="mail"
-          required="true"
-        ></nidoca-inputfield>
-        <nidoca-inputfield
-          .inputfieldType="${InputfieldType.PASSWORD}"
-          label="${I18nService.getUniqueInstance().getValue(
-            "component_register_password"
-          )}"
-          name="password"
-          trailingIcon="vpn_key"
-          minlength="8"
-          required="true"
-        ></nidoca-inputfield>
-        <nidoca-inputfield
-          .inputfieldType="${InputfieldType.CHECKBOX}"
-          label="${I18nService.getUniqueInstance().getValue(
-            "component_register_accept_terms_of_use"
-          )}"
-          name="acceptTermsOfUse"
-          minlength="8"
-          required="true"
-        ></nidoca-inputfield>
-        <nidoca-button
-          text="${I18nService.getUniqueInstance().getValue(
-            "component_register_submit"
-          )}"
-          @nidoca-event-button-clicked="${() => this.register()}"
-        ></nidoca-button>
+   @query('#register-form')
+   formComponent: NidocaForm | undefined;
 
-        <nidoca-typography
-          slot="errorMessages"
-          .typographyType="${TypographyType.OVERLINE}"
-          text="${this.errorMessage}"
-        ></nidoca-typography>
-      </nidoca-form>
-    `;
-  }
+   render() {
+      return html`
+         <nidoca-typography .typographyType="${TypographyType.H4}"
+            >${I18nService.getUniqueInstance().getValue(
+               'nidoca-register-header'
+            )}</nidoca-typography
+         >
+         <nidoca-form id="register-form">
+            <nidoca-inputfield
+               id="email"
+               name="email"
+               .inputfieldType="${InputfieldType.EMAIL}"
+               label="${I18nService.getUniqueInstance().getValue(
+                  'nidoca-register-email'
+               )}"
+               trailingIcon="mail"
+               required="true"
+            ></nidoca-inputfield>
+            <nidoca-inputfield
+               id="repeat-email"
+               name="repeatEmail"
+               .inputfieldType="${InputfieldType.EMAIL}"
+               label="${I18nService.getUniqueInstance().getValue(
+                  'nidoca-register-repeat-email'
+               )}"
+               trailingIcon="mail"
+               required="true"
+            ></nidoca-inputfield>
+            <nidoca-inputfield
+               .inputfieldType="${InputfieldType.PASSWORD}"
+               label="${I18nService.getUniqueInstance().getValue(
+                  'nidoca-register-password'
+               )}"
+               name="password"
+               trailingIcon="vpn_key"
+               minlength="8"
+               required="true"
+            ></nidoca-inputfield>
+            <nidoca-inputfield
+               .inputfieldMode="${InputfieldMode.CLEAN}"
+               .inputfieldType="${InputfieldType.SWITCH}"
+               label="${I18nService.getUniqueInstance().getValue(
+                  'nidoca-register-accept-terms-of-use'
+               )}"
+               name="acceptTermsOfUse"
+               required="true"
+               ><nidoca-typography
+                  .typographyType="${TypographyType.BODY1}"
+                  .typographyAlignment="${TypographyAlignment.LEFT}"
+               >
+                  ${I18nService.getUniqueInstance().getValue(
+                     'nidoca-register-terms-of-use-text'
+                  )}
+                  <nidoca-link href="${this.hrefTermsOfUse}" target="_self"
+                     >${I18nService.getUniqueInstance().getValue(
+                        'nidoca-register-terms-of-use-link'
+                     )}</nidoca-link
+                  >
+               </nidoca-typography></nidoca-inputfield
+            >
+            <nidoca-button
+               text="${I18nService.getUniqueInstance().getValue(
+                  'nidoca-register-submit'
+               )}"
+               @nidoca-event-button-clicked="${() => this.register()}"
+            ></nidoca-button>
 
-  private register() {
-    if (this.emailInputField?.value != this.repeatEmailInputField?.value) {
-      this.errorMessage = I18nService.getUniqueInstance().getValue(
-        "component_register_error_same_email_check"
-      );
-    } else if (this.formComponent?.validate()) {
-      BasicService.getUniqueInstance().dispatchSimpleCustomEvent(
-        this,
-        NidocaRegister.EVENT_SUBMIT,
-        this.getOutputData()
-      );
-    }
-  }
+            <nidoca-visible
+               slot="errorMessages"
+               visibleType="${BasicService.getUniqueInstance().isNotBlank(
+                  this.errorMessage
+               )
+                  ? VisibleType.NORMAL
+                  : VisibleType.HIDE}"
+            >
+               <nidoca-spacer
+                  spacerSize="${SpacerSize.MEDIUM}"
+                  .spacerAlignment="${SpacerAlignment.VERTICAL}"
+               >
+                  <nidoca-typography
+                     .typographyType="${TypographyType.OVERLINE}"
+                     text="${this.errorMessage}"
+                  ></nidoca-typography>
+               </nidoca-spacer>
+            </nidoca-visible>
+         </nidoca-form>
+         <nidoca-spacer
+            spacerSize="${SpacerSize.MEDIUM}"
+            .spacerAlignment="${SpacerAlignment.VERTICAL}"
+         >
+            <nidoca-typography .typographyType="${TypographyType.BODY1}">
+               ${I18nService.getUniqueInstance().getValue(
+                  'nidoca-register-already-have-an-account-text'
+               )}
+               <nidoca-link href="${this.hrefLogin}" target="_self"
+                  >${I18nService.getUniqueInstance().getValue(
+                     'nidoca-register-already-have-an-account-link'
+                  )}</nidoca-link
+               >
+            </nidoca-typography>
+         </nidoca-spacer>
+      `;
+   }
 
-  getOutputData(): NidocaFormOutputData {
-    if (this.formComponent?.isValid()) {
-      let formOutputData: NidocaFormOutputData = this.formComponent.getOutputData();
-      return formOutputData;
-    }
-    return NidocaFormOutputData.prototype;
-  }
+   private register() {
+      this.errorMessage = '';
+      if (this.emailInputField?.value != this.repeatEmailInputField?.value) {
+         this.errorMessage = I18nService.getUniqueInstance().getValue(
+            'nidoca-register-error-same-email-check'
+         );
+      } else if (this.formComponent?.validate()) {
+         BasicService.getUniqueInstance().dispatchSimpleCustomEvent(
+            this,
+            'nidoca-event-register-submit',
+            this.formComponent.getOutputData()
+         );
+      }
+   }
 }
