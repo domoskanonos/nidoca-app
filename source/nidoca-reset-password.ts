87fe3a3a49ada9c7a767d23db71f8e29a260777c
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2020-05-25 14:05:51 +0200

    modified

--- source/nidoca-reset-password.ts
+++ source/nidoca-reset-password.ts
@@ -1,35 +1,19 @@
-import { customElement, html, LitElement, property, query } from "lit-element";
-import {
-  NidocaForm,
-  TypographyType,
-  InputfieldType,
-  NidocaFormOutputData
-} from "@domoskanonos/nidoca-core";
+import { customElement, html, LitElement, query } from "lit-element";
+import { NidocaForm, InputfieldType } from "@domoskanonos/nidoca-core";
 import { BasicService, I18nService } from "@domoskanonos/frontend-basis";
 
 @customElement("nidoca-reset-password")
 export class NidocaResetPassword extends LitElement {
-  static EVENT_RESET_PASSWORD: string = "nidoca-eventreset-password-event";
-
-  @property()
-  changePasswordPath: string = "";
-
   @query("#reset-password-form")
   formComponent: NidocaForm | undefined;
 
   render() {
     return html`
       <nidoca-form id="reset-password-form">
-        <nidoca-typography .typographyType="${TypographyType.H4}"
-          >${I18nService.getUniqueInstance().getValue(
-            "component_reset_password_header"
-          )}</nidoca-typography
-        >
-        <slot></slot>
         <nidoca-inputfield
           .inputfieldType="${InputfieldType.EMAIL}"
           label="${I18nService.getUniqueInstance().getValue(
-            "component_reset_password_email"
+            "nidoca-reset-password-email-label"
           )}"
           trailingIcon="vpn_key"
           minlength="8"
@@ -38,7 +22,7 @@ export class NidocaResetPassword extends LitElement {
         ></nidoca-inputfield>
         <nidoca-button
           text="${I18nService.getUniqueInstance().getValue(
-            "component_reset_password_submit"
+            "nidoca-reset-password-submit"
           )}"
           @click="${() => this.resetPassword()}"
         ></nidoca-button>
@@ -50,10 +34,8 @@ export class NidocaResetPassword extends LitElement {
     if (this.formComponent?.validate()) {
       BasicService.getUniqueInstance().dispatchSimpleCustomEvent(
         this,
-        NidocaResetPassword.EVENT_RESET_PASSWORD,
-        this.formComponent != undefined
-          ? this.formComponent.getOutputData()
-          : NidocaFormOutputData.prototype
+        "nidoca-event-reset-password",
+        this.formComponent.getOutputData()
       );
     }
   }
