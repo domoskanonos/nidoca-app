fb77f6214dbc4a22c6e97cd32eadc51c9f732844
domoskanonos <tgz014vb>
2020-06-12 11:48:42 +0200

    modified

--- source/nidoca-change-password.ts
+++ source/nidoca-change-password.ts
@@ -1,6 +1,18 @@
 import {customElement, html, LitElement, property, query} from 'lit-element';
 import {BasicService, I18nService} from '@domoskanonos/frontend-basis';
 import {NidocaForm, TypographyType, InputfieldType} from '@domoskanonos/nidoca-core';
+import {
+  FlexAlignItems,
+  FlexContainerProperties,
+  FlexDirection,
+  FlexJustifyContent,
+  FlexWrap,
+  GridAlignItems,
+  GridJustifyItems,
+  SpacerAlignment,
+  SpacerSize,
+  TransitionType,
+} from '@domoskanonos/nidoca-core/lib';
 
 @customElement('nidoca-change-password')
 export class NidocaChangePassword extends LitElement {
@@ -20,46 +32,81 @@ export class NidocaChangePassword extends LitElement {
   repeatNewPasswordInputField: HTMLInputElement | undefined;
 
   render() {
-    return html`
-      <nidoca-form id="change-password-form">
-        <nidoca-inputfield
-          id="current-password-inputfield"
-          .inputfieldType="${InputfieldType.PASSWORD}"
-          label="${I18nService.getUniqueInstance().getValue('nidoca-change-password-current-password-label')}"
-          trailingIcon="vpn_key"
-          required="true"
-          name="passwordCurrent"
-        ></nidoca-inputfield>
-        <nidoca-inputfield
-          id="new-password-inputfield"
-          .inputfieldType="${InputfieldType.PASSWORD}"
-          label="${I18nService.getUniqueInstance().getValue('nidoca-change-password-new-password-label')}"
-          trailingIcon="vpn_key"
-          minlength="8"
-          required="true"
-          name="passwordNew"
-        ></nidoca-inputfield>
-        <nidoca-inputfield
-          id="repeat-new-password-inputfield"
-          .inputfieldType="${InputfieldType.PASSWORD}"
-          label="${I18nService.getUniqueInstance().getValue('nidoca-change-password-repeat-new-password-label')}"
-          trailingIcon="vpn_key"
-          minlength="8"
-          required="true"
-          name="passwordNewRepeat"
-        ></nidoca-inputfield>
-        <nidoca-button
-          text="${I18nService.getUniqueInstance().getValue('nidoca-change-password-submit')}"
-          @click="${() => this.changePassword()}"
-        ></nidoca-button>
+    return html`<nidoca-transition .transitionType="${TransitionType.CENTER}">
+      <nidoca-grid-container
+        .gridJustifyItems="${GridJustifyItems.CENTER}"
+        .gridAlignItems="${GridAlignItems.CENTER}"
+        .gridTemplateRows="${['1fr']}"
+        .gridTemplateColumns="${['1fr']}"
+        height="100vh"
+      >
+        <nidoca-flex-container
+          style="width: 400px;"
+          .flexContainerProperties="${[
+            FlexContainerProperties.CONTAINER_WIDTH_100,
+            FlexContainerProperties.SMARTPHONE_MAX_WIDTH,
+            FlexContainerProperties.SMARTPHONE_HORIZONTAL_PADDING,
+          ]}"
+          flexItemBasisValue="auto"
+          .flexDirection="${FlexDirection.COLUMN}"
+          .flexWrap="${FlexWrap.NO_WRAP}"
+          .flexJustifyContent="${FlexJustifyContent.SPACE_AROUND}"
+          .flexAlignItems="${FlexAlignItems.STRETCH}"
+        >
+          <nidoca-icon
+            color="var(--app-color-primary-background)"
+            size="128"
+            icon="vpn_key"
+            .withIconSpace="${false}"
+          ></nidoca-icon>
+          <nidoca-spacer
+            style="text-align:center;"
+            spacerSize="${SpacerSize.MEDIUM}"
+            .spacerAlignment="${SpacerAlignment.VERTICAL}"
+          >
+            <nidoca-typography .typographyType="${TypographyType.H4}"
+              >${I18nService.getUniqueInstance().getValue('nidoca-reset-password_header')}</nidoca-typography
+            >
+          </nidoca-spacer>
+          <nidoca-form id="change-password-form">
+            <nidoca-inputfield
+              id="current-password-inputfield"
+              .inputfieldType="${InputfieldType.PASSWORD}"
+              label="${I18nService.getUniqueInstance().getValue('nidoca-change-password-current-password-label')}"
+              trailingIcon="vpn_key"
+              required="true"
+              name="passwordCurrent"
+            ></nidoca-inputfield>
+            <nidoca-inputfield
+              id="new-password-inputfield"
+              .inputfieldType="${InputfieldType.PASSWORD}"
+              label="${I18nService.getUniqueInstance().getValue('nidoca-change-password-new-password-label')}"
+              trailingIcon="vpn_key"
+              minlength="8"
+              required="true"
+              name="passwordNew"
+            ></nidoca-inputfield>
+            <nidoca-inputfield
+              id="repeat-new-password-inputfield"
+              .inputfieldType="${InputfieldType.PASSWORD}"
+              label="${I18nService.getUniqueInstance().getValue('nidoca-change-password-repeat-new-password-label')}"
+              trailingIcon="vpn_key"
+              minlength="8"
+              required="true"
+              name="passwordNewRepeat"
+            ></nidoca-inputfield>
+            <nidoca-button
+              text="${I18nService.getUniqueInstance().getValue('nidoca-change-password-submit')}"
+              @click="${() => this.changePassword()}"
+            ></nidoca-button>
 
-        <nidoca-typography
-          slot="errorMessages"
-          .typographyType="${TypographyType.OVERLINE}"
-          text="${this.errorMessage}"
-        ></nidoca-typography>
-      </nidoca-form>
-    `;
+            <nidoca-typography
+              slot="errorMessages"
+              .typographyType="${TypographyType.OVERLINE}"
+              text="${this.errorMessage}"
+            ></nidoca-typography> </nidoca-form
+        ></nidoca-flex-container> </nidoca-grid-container
+    ></nidoca-transition>`;
   }
 
   private changePassword() {
