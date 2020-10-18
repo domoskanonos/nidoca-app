48c1a8c92ab3f9de1e4d0c5599f7e72bc83dcb86
domoskanonos <tgz014vb>
2020-10-18 14:09:50 +0200

    modified

--- source/index.ts
+++ source/index.ts
@@ -3,8 +3,6 @@ export * from './nidoca-abstract-component-search-list';
 export * from './nidoca-abstract-template-app';
 export * from './nidoca-abstract-page-search-list';
 export * from './nidoca-i18n-selector';
-export * from './nidoca-data-protection';
 export * from './nidoca-my-data';
 export * from './nidoca-icon-action';
 export * from './nidoca-barcode';
-export * from './nidoca-complex-table';
